a6269f0308dca3d9b6ad45d751860bb02e1f71cb
Sreylynn <sreylynnu@gmail.com>
2019-09-26 23:45:35 -0400

    First commit

---
+++ lastname_testing/src/lastname_testing/thissayshello.java
@@ -0,0 +1,10 @@
+package lastname_testing;
+
+public class thissayshello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This says hello");
+	}
+
+}
