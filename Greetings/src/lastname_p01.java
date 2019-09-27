9b41889a1e22d42447818210c7206149107f39fd
Sreylynn <sreylynnu@gmail.com>
2019-09-26 23:33:25 -0400

    My first commit

--- Greetings/src/lastname_p01.java
+++
@@ -1,9 +0,0 @@
-
-public class lastname_p01 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
---
+++ lastname_01/src/lastname_01/Example01.java
@@ -0,0 +1,19 @@
+package lastname_01;
+
+import java.util.Scanner;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name = scnr.next();
+		
+		System.out.println("Hello, " + name);
+
+	}
+
+}
